b20e959a1d48ce2ac771d812efa8e03bf351df71
medien <medien@10.4.1.29>
2016-04-16 22:44:10 +0700

    14th commit
    addListener untuk button

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -2,32 +2,10 @@ package driver;
 import model.AplikasiKonsol;
 
 public class Driver {
+    public static void main(String[] args) {
+        
     AplikasiKonsol apKonsol = new AplikasiKonsol();
     
-    //testing
-/*    apKonsol.addPerusahaan("huehue", "hahahaha", "092211", "ufufu@abc.com", "tidakAda.com");
-    apKonsol.addPerusahaan("nainai", "lalala", "yohohoho", "03891321", "huhuhuuu");
-    apKonsol.addPerusahaan("ushishishi", "ehehe", "---", "123", "huhuhuuu");
-//    apKonsol.addPerusahaan("ijfiwea", "ihansd", "02133", "laiea", "kekeke");
 
-//    apKonsol.deletePerusahaan("huehue");
-//    int found = apKonsol.findPerusahaan("nainai");
-//    System.out.println("Data ada pada array ke-"+found);
-//    apKonsol.delPerusahaan(found);
-
-    apKonsol.deletePerusahaan("ushishishi");
-    for(int i=0; i<3; i++){
-        if (apKonsol.perusahaan[i] == null)
-            System.out.println("kosong");
-        else
-            apKonsol.perusahaan[i].printBio();
-    }
-    
-    public String yay(){
-        return ("hello world");
-    }
-
-    //apKonsol.callMainManu();
-*/
-    
+    }   
 }
---
+++ src/viewApp/viewPelamarTerdaftar.java
@@ -0,0 +1,73 @@
+package viewApp;
+
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+public class viewPelamarTerdaftar extends javax.swing.JPanel {
+
+    public viewPelamarTerdaftar() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtViewListPelamar = new javax.swing.JTextArea();
+        btnBack2 = new javax.swing.JButton();
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setText("List Pelamar (yang terdaftar pada sistem) :");
+
+        txtViewListPelamar.setColumns(20);
+        txtViewListPelamar.setRows(5);
+        jScrollPane1.setViewportView(txtViewListPelamar);
+
+        btnBack2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        btnBack2.setText("Kembali");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(99, 99, 99)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnBack2, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jLabel1)))
+                .addContainerGap(113, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(59, 59, 59)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
+                .addComponent(btnBack2, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(25, 25, 25))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBack2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea txtViewListPelamar;
+    // End of variables declaration//GEN-END:variables
+
+    public void addListener(ActionListener e){
+        btnBack2.addActionListener(e);
+    }
+    
+    public JButton getBack2(){
+        return btnBack2;
+    }
+    
+    
+}
